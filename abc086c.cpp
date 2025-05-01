0b10f899abd96873dee9964baec1ac5b176bf6e1
ckp-koiken <kentaro.koi0710@gmail.com>
2025-05-01 16:18:17 +0900

    add interpret(part)

--- abc086c.cpp
+++ abc086c.cpp
@@ -8,15 +8,22 @@ vector<int> T(101010), X(101010), Y(101010);
 #define yes "Yes"
 #define no "No"
 
+// (px, py)から(X.at(i), Y.at(i))へ時間T.at(i)で遷移可能かを判定する
 string solve() {
   int pt = 0, px = 0, py = 0;
   for (int i = 0; i < N; i++) {
+    // 2つの座標間を距離dで表す
     int d = abs(px - X.at(i)) + abs(py - Y.at(i));
+    // 使える時間をdtで表す
     int dt = T.at(i) - pt;
+
+    // 使える時間の方が距離よりも短ければ実現不可能
+    // 最短距離で移動しても間に合わない
     if (dt < d) {
       return no;
     }
 
+    // TODO: dt-dが偶数であるなら移動できる
     if ((dt - d) % 2 == 1) {
       return no;
     }
